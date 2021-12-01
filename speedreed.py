46499f9146616563e9cfa982e174bea3b1badab6
Xander Tedder <xandertedder7@gmail.com>
2021-11-30 22:24:49 -0500

    added -m, --margin
    set limits to how many chars can be printed on each line, only works when reading from files, no stdin support yet!

--- speedreed.py
+++ speedreed.py
@@ -1,11 +1,12 @@
 import sys
 from time import sleep
+from os.path import exists
 
 __author__ = "xandertedder7@gmail.com"
 __version__ = "v1.0"
 
 R_MODE = "r"
-ARGUMENTS = ["-h", "--help", "-t", "--time", "-R", "--reverse"]
+ARGUMENTS = ["-h", "--help", "-t", "--time", "-R", "--reverse", "-m", "--margin"]
 HELP = """
 Usage: speedreed [FILE, -]... [OPTION]...
 
@@ -13,6 +14,7 @@ Concatenate FILE(s), or standard input, to standard output.
 With no FILE, or when FILE is -, read standard input. (quickly!)
 
   -t  --time\tSet delay between chars, (default .01) 
+  -m  --margin\tSet margins or rather limit of chars before newline (only works with files currently)
   -R  --reverse\tReverse the given standard input or file contents
   -V  --version\tPrint version and exit
   -h  --help\tShow this help message and exit
@@ -27,37 +29,43 @@ Examples:
 # set defaults for T_SLEEP & FILE
 defaults = {
         "FILE": False,
-        "T_SLEEP": .01,
+        "margin": None,
+        "T_SLEEP": .01
 }
 
-def SpeedReed(string, T_SLEEP):
+
+def SpeedReed(string):
     index = 0
 
     while index < len(string):
         sys.stdout.write(string[index])
         sys.stdout.flush()
-        sleep(T_SLEEP)
+        sleep(defaults["T_SLEEP"])
 
         index += 1
 
-def SpeedReadSTDIN(T_SLEEP):
+
+def SpeedReadSTDIN():
     # read from the standard in
     stdin_c = sys.stdin.read()
-    SpeedReed(stdin_c, T_SLEEP)
+    SpeedReed(stdin_c)
 
-def SpeedReedSTDINReverse(T_SLEEP):
+
+def SpeedReedSTDINReverse():
     stdin_c_rev = sys.stdin.read()[::-1]
-    SpeedReed(stdin_c_rev, T_SLEEP)
+    SpeedReed(stdin_c_rev)
     sys.stdout.write("\n")
 
-def SpeedReedFile(FP, T_SLEEP):
+
+def SpeedReedFile(FP):
     # read from a file with mode r (Read)
     with open(FP, R_MODE) as reed:
         reed = reed.read()
 
-    SpeedReed(reed, T_SLEEP)
+    SpeedReed(reed)
 
-def SpeedReedFileReverse(FP, T_SLEEP):
+
+def SpeedReedFileReverse(FP):
     with open(FP, R_MODE) as rev_reed:
         rev_reed = rev_reed.read()[::-1]
 
@@ -65,9 +73,50 @@ def SpeedReedFileReverse(FP, T_SLEEP):
     while index < len(rev_reed):
         sys.stdout.write(rev_reed[index])
         sys.stdout.flush()
-        sleep(T_SLEEP)
+        sleep(defaults["T_SLEEP"])
 
         index += 1
+
+
+
+def ReedMargin(fp, margin, f=False):
+    # end the line early with a newline if length is hit, reset length parameter.
+    index = 0
+    if f is True:
+        with open(fp, R_MODE) as mreed:
+            mreed = mreed.read()
+    else:
+        mreed = sys.stdin.read()
+
+    while index < len(mreed):
+        if index % margin == 0:
+            sys.stdout.write("\n")
+
+        sys.stdout.write(mreed[index])
+        sys.stdout.flush()
+        sleep(defaults["T_SLEEP"])
+            
+        index += 1
+
+
+def ReedFiles(args, reverse=False, margin=False, stdin_margin=False):
+    for ex in args[1::]:
+        if exists(ex) == True:
+            if reverse:
+                SpeedReedFileReverse(ex)
+            elif margin:
+                if defaults["margin"] == None or defaults["margin"] == 0:
+                    sys.stdout.write("Warning: margin value cannot be None, 0, or greater than the file itself!\n")
+                    exit(1)
+
+                else:
+                    ReedMargin(ex, defaults["margin"], f=defaults["FILE"])
+
+            else:
+                SpeedReedFile(ex)
+        else:
+            continue
+
 
 if __name__ == "__main__":
     try:
@@ -76,21 +125,21 @@ if __name__ == "__main__":
             exit(0)
 
         if "-h" in sys.argv or "--help" in sys.argv:
-            SpeedReed(HELP, defaults["T_SLEEP"])
+            SpeedReed(HELP)
             exit(0)
 
         if "-t" in sys.argv:
             try:
                 defaults["T_SLEEP"] = float(sys.argv[sys.argv.index("-t")+1])
             except Exception: 
-                sys.stdout.write(f"Warning: missing operand for -t, applying default {defaults['T_SLEEP']}\n\n")
+                sys.stdout.write(f"Warning: missing operand for -t, applying default of {defaults['T_SLEEP']}\n\n")
                 defaults["T_SLEEP"] = .01
 
         elif "--time" in sys.argv:
             try:
                 defaults["T_SLEEP"] = float(sys.argv[sys.argv.index("--time")+1])
             except Exception:
-                sys.stdout.write(f"Warning: missing operand for --time, applying default {defaults['T_SLEEP']}\n\n")
+                sys.stdout.write(f"Warning: missing operand for --time, applying default of {defaults['T_SLEEP']}\n\n")
                 defaults["T_SLEEP"] = .01
 
         
@@ -98,15 +147,36 @@ if __name__ == "__main__":
             
             if "-R" in sys.argv or "--reverse" in sys.argv:
                 defaults["FILE"] = True
-                SpeedReedFileReverse(sys.argv[1], defaults["T_SLEEP"])
-            else:
+                ReedFiles(sys.argv, reverse=True)
+            
+            if "-m" in sys.argv:
                 defaults["FILE"] = True
-                SpeedReedFile(sys.argv[1], defaults["T_SLEEP"])
-    
-        else:
-            if "-R" in sys.argv or "--reverse" in sys.argv:
-                SpeedReedSTDINReverse(defaults["T_SLEEP"])
+                try:
+                    defaults["margin"] = int(sys.argv[sys.argv.index("-m")+1])
+                    ReedFiles(sys.argv, margin=True)
+                    exit(0)
+                except Exception:
+                    sys.stdout.write(f"Warning: missing operand for -m, applying default of {defaults['margin']}\n\n")
+                    defaults["margin"] = None
+
+            if "--margin" in sys.argv:
+                defaults["FILE"] = True
+                try:
+                    defaults["margin"] = int(sys.argv[sys.argv.index("--margin")+1])
+                    ReedFiles(sys.argv, margin=True)
+                    exit(0)
+                except Exception:
+                    sys.stdout.write(f"Warning: missing operand for --margin, applying default of {defaults['margin']}\n\n")
+                    defaults["margin"] = None
+                    
             else:
-                SpeedReadSTDIN(defaults["T_SLEEP"])
+                ReedFiles(sys.argv)
+                exit(0)
+        else:
+            SpeedReadSTDIN()
+
+        if "-R" in sys.argv or "--reverse" in sys.argv:
+            SpeedReedSTDINReverse()
+
     except KeyboardInterrupt as e:
         print(str(e)+"\n")
